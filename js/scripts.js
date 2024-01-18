01d88989cfc32336ac6b815a39d4a9c6698d63df
romywritescode <148618153+romywritescode@users.noreply.github.com>
2024-01-18 11:42:56 +0100

    hamburger menu
    I added all the code from the lesson, why cant I see the menu?

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
-console.log("Hello world")
+<li><a href="javascript:void(0);" class="icon" onclick="menuToggle()"><i class="fas fa-bars"></i></a></li>
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
