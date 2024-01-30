5e012324dbdfa4406ec73d4c2921b190ccaf3aed
romywritescode <148618153+romywritescode@users.noreply.github.com>
2024-01-30 07:56:13 +0100

    changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-<li><a href="javascript:void(0);" class="icon" onclick="menuToggle()"><i class="fas fa-bars"></i></a></li>
 function menuToggle() {
     var x = document.getElementById('myNavtoggle');
     if (x.className === 'navtoggle') {
